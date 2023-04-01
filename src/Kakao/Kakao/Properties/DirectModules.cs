6d10e6151d4ef5897e6bb763932e8fe577f73b1a
CheolYong <cjfdyd4257@gmail.com>
2023-04-01 14:26:14 +0900

    Updates

---
+++ src/Kakao/Kakao/Properties/DirectModules.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Kakao.Properties
+{
+    internal class DirectModules : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //IRegionManager regionManager = containerProvider.Resolve<IRegionManager>();
+            //regionManager.RegisterViewWithRegion("MainRegion", "LoginContent");
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
